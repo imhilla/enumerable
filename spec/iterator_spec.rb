2093bff416bfa8398766c8b9fe84f4db27bf2a7f
imhilla <hillaryodhiambo282@gmail.com>
2020-06-08 11:51:54 +0300

    add test for not modify class

--- spec/iterator_spec.rb
+++ spec/iterator_spec.rb
@@ -7,6 +7,10 @@ describe Enumerable do
  it 'enumerable when no block is given' do
   expect((1..4).my_each).to be_instance_of(Enumerator)
  end
+
+ it 'should not modify an array' do
+  expect((number.my_each {|x| x * x})).to match(number)
+ end
  
  end
 end
