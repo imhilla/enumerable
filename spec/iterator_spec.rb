fd78772d5a0f27a1d73a39a8b8865ef4d86e233a
imhilla <hillaryodhiambo282@gmail.com>
2020-06-08 11:21:15 +0300

    add rspec and dir

--- spec/iterator_spec.rb
+++ spec/iterator_spec.rb
@@ -1,7 +1,9 @@
 require './enumerables.rb'
 describe Enumerable do
   let(:number) { [1, 2, 3, 4, 2] }
-  let(:string) { %w[cat bear rat] }
+  let(:string) { %w[cat bear rat] } 
+
  describe "#my_each" do
+ 
  end
 end
